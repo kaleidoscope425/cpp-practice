8e9b7e5e08dc2327ee9b87f9061d1152e1ec262b
Kaleidoscope425 <kaleidoscope425@gmail.com>
2026-04-04 11:04:22 -0500

    Add D5-Sum of Digits program

---
+++ D5_Sum of Digits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    while (number != 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    cout << "Sum of digits: " << sum << endl;
+
+    return 0;
+}
